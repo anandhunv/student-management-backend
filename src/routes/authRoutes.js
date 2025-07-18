18db59b03a29f771ee1cc371611a928b089035d9
anandhunv <anandhunv05@gmail.com>
2025-07-19 01:24:49 +0530

    some changes at routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,14 +7,13 @@ import {
   getProfile
 } from "../controllers/authController.js"
 import { protect } from "../middleware/authMiddleware.js"
-import { loginLimiter, refreshLimiter } from "../middleware/rateLimiters.js"
 
 const router = express.Router()
 
 router.post("/signup", signup)        // for Super Admin (initial setup only)
-router.post("/login",loginLimiter, login)
+router.post("/login", login)
 router.post("/logout", logout)
-router.get("/refresh",refreshLimiter, refresh)
+router.get("/refresh", refresh)
 router.get("/profile", protect, getProfile)
 
 export default router
